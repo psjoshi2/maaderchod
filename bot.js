80ea81b73afd1e88ce05b34bc8361486b3dd1554
psjoshi2 <paarth_joshi@hotmail.com>
2017-03-14 15:44:54 -0500

    regex fix again

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\finger$/;
 
-  if(request.text && (botRegex.test(request.text))
+  if(request.text && botRegex.test(request.text))
   {
     this.res.writeHead(200);
     postMessage();
